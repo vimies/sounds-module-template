e8e31622e977b518310487f5906709ea162d00c7
Morniak <guillaume.cledat@gmail.com>
2017-04-28 16:04:41 +0200

    First commit with ViewControllerModule template

---
+++ Sounds Modules/ViewController.xctemplate/___FILEBASENAME___Controller.swift
@@ -0,0 +1,18 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import Foundation
+
+class ___FILEBASENAMEASIDENTIFIER___Controller {
+    // MARK: - Injected vars
+    weak var view: ___FILEBASENAMEASIDENTIFIER___ViewController!
+    
+    func doSomething() {
+        view.displaySomething()
+    }
+}
---
+++ Sounds Modules/ViewController.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class ___FILEBASENAMEASIDENTIFIER___ViewController: UIViewController {
+    
+    // MARK: - Injected vars
+    
+    var controller: ___FILEBASENAMEASIDENTIFIER___Controller!
+}
+
+
+// MARK: - View lifecycle
+
+extension ___FILEBASENAMEASIDENTIFIER___ViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.controller.doSomething()
+    }
+}
+
+
+// MARK: - Public methods
+
+extension ___FILEBASENAMEASIDENTIFIER___ViewController {
+    func displaySomething() {
+    }
+}
+
+
+// MARK: - Private methods
+
+fileprivate extension ___FILEBASENAMEASIDENTIFIER___ViewController {
+    
+}
